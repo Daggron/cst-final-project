c72301209f858c49293856ad716def8ab4765d14
Abhay Sharma <returnofking04@gmail.com>
2019-04-15 22:41:05 +0530

    Login Page Created

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function thanos() {
+
+    setTimeout(function () {
+        window.location.replace("login.html");
+
+    },3000)
+
+}
+
+function change() {
+    window.location.replace("login.html");
+
+}
+
+
