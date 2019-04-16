58a1ad4b70bd0891250ea30c6a227a292fae14af
Abhay Sharma <returnofking04@gmail.com>
2019-04-17 01:17:02 +0530

    New Sections Added

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 function thanos() {
 
     setTimeout(function () {
-        window.location.replace("login.html");
+        window.location.replace("index.html");
 
     },3000)
 
 }
 
 function change() {
-    window.location.replace("login.html");
+    window.location.replace("index.html");
 
 }
 
 
+document.getElementsByTagName('video')[0].play();
+document.getElementById('video').play();
